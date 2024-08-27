f06af3c64976d2b6222280679c0ea940ea808c50
DarrachBarneveld <130994206+DarrachBarneveld@users.noreply.github.com>
2024-08-28 00:57:55 +0100

    Updated Whoami route

--- auth/urls.py
+++ auth/urls.py
@@ -6,5 +6,5 @@ from . import views
 urlpatterns = [
     path('login', views.MyTokenObtainPairView.as_view(), name='token_obtain_pair'),
     path('register', views.RegisterView.as_view(), name='register'),
-    path('user', views.FetchUser.as_view(), name='user'),
+    path('whoami', views.FetchUser.as_view(), name='whoami'),
 ]
